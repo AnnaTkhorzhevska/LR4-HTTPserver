6273bdd28635d81f886695e48a9dea91b00f13e6
Your Name <you@example.com>
2024-12-08 17:40:35 +0200

    Hotfix: FuncA calculates sum of first 3 elements

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "func.h"
+
+int main() {
+    Func myFunc;
+    double x;
+    int n;
+
+    std::cout << "Enter the value of x (|x| < 1): ";
+    std::cin >> x;
+    std::cout << "Enter the number of terms (n): ";
+    std::cin >> n;
+
+    double result = myFunc.FuncA(n, x);
+    std::cout << "arth(" << x << ") ≈ " << result << std::endl;
+
+    return 0;
+}
